196b7b9ff92e017bfd0dab955a6d149ae038f7b9
Rob Blackbourn <rob.blackbourn@gmail.com>
2021-07-08 11:11:38 +0100

    Added py.typed

--- jetblack_metrics/monitor.py
+++ jetblack_metrics/monitor.py
@@ -8,19 +8,24 @@ from .metrics import Metric
 
 TMetric = TypeVar('TMetric', bound=Metric)
 
+
 @contextmanager
 def monitor(metric: TMetric) -> Iterator[TMetric]:
     """Monitor an operation
 
-    :param metric: The metric to update in the monitored session.
-    :type metric: TMetric
-    :return: The context managed metric
-    :rtype: Iterator[TMetric]
+    Args:
+        metric (TMetric): The metric to update in the monitored session.
+
+    Raises:
+        metric.error: If the enclosed block raises an exception.
+
+    Yields:
+        Iterator[TMetric]: The context managed metric
     """
     try:
         metric.on_enter()
         yield metric
-    except Exception as error: # pylint: disable=broad-except
+    except Exception as error:  # pylint: disable=broad-except
         metric.error = error
     finally:
         metric.on_exit()
